788334cc6a8c377e4f286d3de46e0244f5541ed1
jeffmc <jeffmc@localhost.localdomain>
2014-03-24 15:51:56 -0400

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+openshift-postgres-client-cartridge
+=============================
+
+This OpenShift cartridge, developed by Crunchy Data Solutions (www.crunchydatasolutions.com).
+
+This cartridge sets an Openshift application environment up
+to help in accessing the deployed Openshift postgres configuration
+as provided by the Crunchy postgres Cartridge.
+
---
+++ bin/create-tunnel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+echo "create-tunnel called for " $PG_NODE_TYPE
+
+if [ "$PG_NODE_TYPE" == "master" ]; then
+nohup ssh -o UserKnownHostsFile=.openshift_ssh/known_hosts \
+-i ~/app-root/data/pg_rsa_key \
+-N -L \
+$OPENSHIFT_PG_HOST:15000:$PG_STANDBY_IP:5432 \
+$PG_STANDBY_USER@$PG_STANDBY_DNS &> /dev/null &
+fi
+
+if [ "$PG_NODE_TYPE" == "standby" ]; then
+nohup ssh -o UserKnownHostsFile=.openshift_ssh/known_hosts \
+-i ~/app-root/data/pg_rsa_key \
+-N -L \
+$OPENSHIFT_PG_HOST:15000:$PG_MASTER_IP:5432 \
+$PG_MASTER_USER@$PG_MASTER_DNS &> /dev/null &
+fi
+
