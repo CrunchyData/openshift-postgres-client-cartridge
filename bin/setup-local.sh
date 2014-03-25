8f8e97b7da330358ca2f755b2323bd3cf6e70b93
jeffmc <jeffmc@localhost.localdomain>
2014-03-25 14:55:59 -0400

    fix

--- bin/setup-local.sh
+++
@@ -1,5 +0,0 @@
-rhc scp jbossews upload /tmp/pg_known_hosts .openshift_ssh/known_hosts
-
-rhc scp jbossews upload ~/code/openshift-postgres-cartridge/bin/pg_rsa_key app-root/data
-
-
---
+++ bin/upload-pg-credentials.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "this script uploads security credentials to the app target"
+echo "this provides connectivity to the postgres cluster from the target app"
+
+echo "enter name of target app:"
+read targetapp
+
+echo "enter full path of pg_known_hosts file:"
+read knownpath
+
+echo "enter full path of pg_rsa_key:"
+read keypath
+
+rhc scp $targetapp upload $knownpath .openshift_ssh/known_hosts
+
+rhc scp $targetapp upload $keypath app-root/data
+
+
