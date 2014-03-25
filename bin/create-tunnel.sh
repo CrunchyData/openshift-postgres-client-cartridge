e8d03858e00ea37ed39d3e5fb0dcafe17d0424fc
jeffmc <jeffmc@localhost.localdomain>
2014-03-25 12:19:00 -0400

    fix

--- bin/create-tunnel.sh
+++ bin/create-tunnel.sh
@@ -4,15 +4,18 @@
 # create a tunnel to both the master and the standby server
 #
 
+internalIP=`env | grep OPENSHIFT | grep IP`
+echo $internalIP
+
 nohup ssh -o UserKnownHostsFile=~/.openshift_ssh/known_hosts \
 -i ~/app-root/data/pg_rsa_key \
 -N -L \
-$OPENSHIFT_JBOSSEWS_IP:15001:$PG_STANDBY_IP:5432 \
+$internalIP:15001:$PG_STANDBY_IP:5432 \
 $PG_STANDBY_USER@$PG_STANDBY_DNS &> /dev/null &
 
 nohup ssh -o UserKnownHostsFile=~/.openshift_ssh/known_hosts \
 -i ~/app-root/data/pg_rsa_key \
 -N -L \
-$OPENSHIFT_JBOSSEWS_IP:15000:$PG_MASTER_IP:5432 \
+$internalIP:15000:$PG_MASTER_IP:5432 \
 $PG_MASTER_USER@$PG_MASTER_DNS &> /dev/null &
 
