9caee3b9a8fb396aab4036eee15881a73177a5e4
jeffmc <jeffmc@localhost.localdomain>
2014-04-01 16:11:32 -0400

    fix

--- bin/create-tunnel.sh
+++ bin/create-tunnel.sh
@@ -10,12 +10,12 @@
 nohup ssh -o UserKnownHostsFile=~/.openshift_ssh/known_hosts \
 -i ~/.openshift_ssh/pg_rsa_key \
 -N -L \
-$OPENSHIFT_PGCLIENT_HOST:15001:$PG_STANDBY_IP:5432 \
+$OPENSHIFT_PGCLIENT_HOST:$PGCLIENT_STANDBY_PORT:$PG_STANDBY_IP:$PGCLIENT_REMOTE_PG_PORT \
 $PG_STANDBY_USER@$PG_STANDBY_DNS &> /dev/null &
 
 nohup ssh -o UserKnownHostsFile=~/.openshift_ssh/known_hosts \
 -i ~/.openshift_ssh/pg_rsa_key \
 -N -L \
-$OPENSHIFT_PGCLIENT_HOST:15000:$PG_MASTER_IP:5432 \
+$OPENSHIFT_PGCLIENT_HOST:$PGCLIENT_MASTER_PORT:$PG_MASTER_IP:$PGCLIENT_REMOTE_PG_PORT \
 $PG_MASTER_USER@$PG_MASTER_DNS &> /dev/null &
 
