eea48498e88a8daedbd03028929236dc5d278ac4
jeffmc <jeffmc@localhost.localdomain>
2014-04-02 13:50:07 -0400

    fix

--- bin/setup-jbossews.sh
+++ bin/setup-jbossews.sh
@@ -3,7 +3,7 @@
 source $OPENSHIFT_CARTRIDGE_SDK_BASH
 
 contextfile=$OPENSHIFT_REPO_DIR/.openshift/config/context.xml
-jbossews=~/pgclient/conf/tomcat7
+jbossews=$OPENSHIFT_PGCLIENT_DIR/conf/tomcat7
 newdatasources=$jbossews/context.xml
 
 if [ -n "$OPENSHIFT_JBOSSEWS_IP" ];
@@ -16,7 +16,7 @@ then
                 sed '/<\/Context/i MARKER' $contextfile  | sed -e '/MARKER/r '$newdatasources -e '/MARKER/d'  >> $jbossews/pgclient-context.xml
                 cp $contextfile $contextfile.bak
                 cp $jbossews/pgclient-context.xml $contextfile
-				cd ~/jbossews/bin && ./control restart
+				$OPENSHIFT_JBOSSEWS_DIR/bin/control restart
         else
                 client_result "jbossews context.xml not found"
         fi
