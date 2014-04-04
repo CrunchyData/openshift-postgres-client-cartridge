c074ca94ee4ab7d6b96f3bda87afb917337656f3
jeffmc <jeffmc@localhost.localdomain>
2014-04-04 10:28:36 -0400

    fix

--- bin/upgrade-pg.sh
+++ bin/upgrade-pg.sh
@@ -8,12 +8,22 @@ read targetapp
 
 echo "enter full path of postgres archive file to upload:"
 read knownpath
+thebase=`basename $knownpath`
+echo "base=" $thebase
 
 rhc scp $targetapp upload $knownpath app-root/data
 
 echo "upgrading the crunchy pg database version..."
 
-rhc ssh $targetapp --command pg/bin/upgrade.sh
+echo "shutting down the postgres database..."
+
+rhc cartridge-stop crunchydatasolutions-pg-1.0 --app $targetapp
+
+rhc ssh -a $targetapp --command 'pg/bin/perform-upgrade.sh app-root/data/$thebase'
+
+echo "starting the postgres database...."
+
+rhc cartridge-start crunchydatasolutions-pg-1.0 --app $targetapp
 
 echo "the upgrade is completed, log into your app to verify!"
 
