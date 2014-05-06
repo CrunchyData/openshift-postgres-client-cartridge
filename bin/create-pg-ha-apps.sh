669acb490efe3477a9f967e5d3e01d8ef6805b75
jeffmc <jeffmc@localhost.localdomain>
2014-05-06 09:21:11 -0400

    dev

--- bin/create-pg-ha-apps.sh
+++ bin/create-pg-ha-apps.sh
@@ -30,39 +30,13 @@ if [[ -z $webframework ]];
     then webframework=$defaultwebframework;
 fi
 
-# clean up master if already exists
-
-pgmasterhostname=$pgmastername-$openshiftdomain.$domainname
-ssh-keygen -R $pgmasterhostname
-rhc app-delete -a $pgmastername --confirm
-/bin/rm -rf $pgmastername
-
-rhc create-app -a $pgmastername -t $webframework
-echo $pgmastername " created..."
-
-echo "adding Crunchy postgres cartridge to "$pgmastername
-rhc add-cartridge crunchydatasolutions-pg-1.0 -a $pgmastername --env PG_NODE_TYPE=master
-rhc ssh -a $pgmastername --command '~/pg/bin/control stop'
-echo "adding Crunchy HA cartridge to "$pgmastername
-rhc add-cartridge crunchydatasolutions-pgclient-1.0 -a $pgmastername
-
-#force a key to be added to your local known_hosts file
-rhc ssh -a $pgmastername --command 'date'
-
-rm ./pg_known_hosts
-touch ./pg_known_hosts
-chmod 600 ./pg_known_hosts
-
-#now, get the keys for the pg servers from the local_hosts file
-#and build a custom known_hosts file
-ssh-keygen -F $pgmasterhostname >> ./pg_known_hosts
-
-
-# create a number of standby apps up to max of 5
+# define a number of standby apps up to max of 5
 
 standbyarray=()
-
+fqdnstandbyarray=()
+standbyportarray=()
 COUNTER=0
+standbyportnum=15001
 while [ $COUNTER -lt 5 ] 
 do
 		defaultstandbyname="pgstandby"$COUNTER
@@ -76,13 +50,63 @@ do
 			break
 		fi
 
+		standbyarray=( "${standbyarray[@]}" $defaultstandbyname )
+		fqdnstandbyarray=( "${fqdnstandbyarray[@]}" $defaultstandbyname-$openshiftdomain.$domainname )
+		standbyportarray=( "${standbyportarray[@]}" $standbyportnum )
+
+		let "COUNTER=$COUNTER+1"
+		let "standbyportnum=$standbyportnum+1"
+done
+echo "standby servers are "${standbyarray[*]}
+echo "fqdn standby servers are "${fqdnstandbyarray[*]}
+echo "standby ports are "${standbyportarray[*]}
+
+# clean up master if already exists
+
+pgmasterhostname=$pgmastername-$openshiftdomain.$domainname
+ssh-keygen -R $pgmasterhostname
+rhc app-delete -a $pgmastername --confirm
+/bin/rm -rf $pgmastername
+
+rhc create-app -a $pgmastername -t $webframework
+echo $pgmastername " created..."
+pgmasteruser=`rhc ssh -a $pgmastername 'echo $USER' 2> /dev/null`
+echo $pgmastername USER is $pgmasteruser
+pgmasterdns=$pgmastername-$openshiftdomain.$domainname
+echo $pgmasterdns is PG_MASTER_DNS
+
+echo "adding Crunchy postgres cartridge to "$pgmastername
+rhc add-cartridge crunchydatasolutions-pg-1.0 -a $pgmastername --env PG_NODE_TYPE=master  JEFF_PG_STANDBY_DNS_LIST='${fqdnstandbyarray[*]}' JEFF_PG_STANDBY_PORT_LIST='${standbyportarray[*]}'
+pgmasterip=`rhc ssh -a $pgmastername 'echo $OPENSHIFT_PG_HOST' 2> /dev/null`
+echo PG_MASTER_IP is $pgmasterip
+rhc ssh -a $pgmastername --command '~/pg/bin/control stop'
+
+echo "adding Crunchy HA cartridge to "$pgmastername
+rhc add-cartridge crunchydatasolutions-pgclient-1.0 -a $pgmastername 
+
+#force a key to be added to your local known_hosts file
+rhc ssh -a $pgmastername --command 'date'
+
+rm ./pg_known_hosts
+touch ./pg_known_hosts
+chmod 600 ./pg_known_hosts
+
+#now, get the keys for the pg servers from the local_hosts file
+#and build a custom known_hosts file
+ssh-keygen -F $pgmasterhostname >> ./pg_known_hosts
+
+
+
+cnt=${standbyarray[@]}
+for standbyapp in  ${standbyarray[*]}
+do
 		# clean up previous app with same name if exists
-		pgstandbyhostname=$pgstandbyname-$openshiftdomain.$domainname
+		pgstandbyhostname=$standbyapp-$openshiftdomain.$domainname
 		ssh-keygen -R $pgstandbyhostname
 		rhc app-delete -a $pgstandbyname --confirm
 		/bin/rm -rf $pgstandbyname
 		echo "creating "$pgstandbyname
-		rhc create-app -a $pgstandbyname -t $webframework -g standby
+		rhc create-app -a $pgstandbyname -t $webframework -g standby --env JEFF_PG_MASTER_USER=$pgmasteruser JEFF_PG_MASTER_DNS=$pgmasterdns JEFF_PG_MASTER_IP=$pgmasterip
 		echo $pgstandbyname " created..."
 		echo "adding Crunchy postgres cartridge to "$pgstandbyname
 		rhc add-cartridge crunchydatasolutions-pg-1.0 -a $pgstandbyname --env PG_NODE_TYPE=standby
@@ -92,11 +116,8 @@ do
 		ssh-keygen -F $pgstandbyhostname >> ./pg_known_hosts
 		standbyarray=( "${standbyarray[@]}" $defaultstandbyname )
 
-
-		let "COUNTER=$COUNTER+1"
 done
 
-echo "standby servers are "${standbyarray[*]}
 
 #now, copy the pg known_hosts to the targets
 rhc scp $pgmastername upload ./pg_known_hosts .openshift_ssh/known_hosts
