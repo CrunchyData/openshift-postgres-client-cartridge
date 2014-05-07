84fc4d170676fd6295dc65db8339e06288b0910b
jeffmc <jeffmc@localhost.localdomain>
2014-05-07 14:17:27 -0400

    fix

--- bin/create-tunnel.sh
+++ bin/create-tunnel.sh
@@ -8,15 +8,34 @@
 #internalIP=`env | grep OPENSHIFT | grep IP | cut -d"=" -f2`
 #echo $internalIP
 
-nohup ssh -o UserKnownHostsFile=~/.openshift_ssh/known_hosts \
--i ~/.openshift_ssh/pg_rsa_key \
--N -L \
-$OPENSHIFT_PGCLIENT_HOST:$PGCLIENT_STANDBY_PORT:$PG_STANDBY_IP:$PGCLIENT_REMOTE_PG_PORT \
-$PG_STANDBY_USER@$PG_STANDBY_DNS &> /dev/null &
+echo dns list $JEFF_PG_STANDBY_DNS_LIST
+dnsarray=($JEFF_PG_STANDBY_DNS_LIST)
+echo user list $JEFF_PG_STANDBY_USER_LIST
+userarray=($JEFF_PG_STANDBY_USER_LIST)
+echo port list $JEFF_PG_STANDBY_PORT_LIST
+portarray=($JEFF_PG_STANDBY_PORT_LIST)
+#
+# create tunnel to each standby postgres server
+#
 
+idx=0
+
+for standby in ${dnslist[*]};
+do
+	nohup ssh -o UserKnownHostsFile=~/.openshift_ssh/known_hosts \
+-i ~/.openshift_ssh/pg_rsa_key \
+-N -L \
+$OPENSHIFT_PGCLIENT_HOST:${portarray[idx]}:${dnsarray[idx]}:$PGCLIENT_REMOTE_PG_PORT \
+${userarray[idx]}@${dnsarray[idx]} &> /dev/null &
+	let "idx=$idx+1"
+done
+
+#
+# create a tunnel to the master postgres server
+#
 nohup ssh -o UserKnownHostsFile=~/.openshift_ssh/known_hosts \
 -i ~/.openshift_ssh/pg_rsa_key \
 -N -L \
-$OPENSHIFT_PGCLIENT_HOST:$PGCLIENT_MASTER_PORT:$PG_MASTER_IP:$PGCLIENT_REMOTE_PG_PORT \
+$OPENSHIFT_PGCLIENT_HOST:$PGCLIENT_MASTER_PORT:$PGCLIENT_MASTER_HOST:$PGCLIENT_REMOTE_PG_PORT \
 $PG_MASTER_USER@$PG_MASTER_DNS &> /dev/null &
 
